88f0bc5be516d74adc456f2e583058f851e0a64c
Daniel McRobb <dmcrobb@one.ai>
2024-04-19 20:30:10 -0400

    Added RIPE asn.txt handling.

---
+++ classes/tests/TestRipeAsnTxt.cc
@@ -0,0 +1,91 @@
+//===========================================================================
+// @(#) $DwmPath$
+//===========================================================================
+//  Copyright (c) Daniel W. McRobb 2024
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The names of the authors and copyright holders may not be used to
+//     endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+//  IN NO EVENT SHALL DANIEL W. MCROBB BE LIABLE TO ANY PARTY FOR
+//  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+//  INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF DANIEL W. MCROBB HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+//
+//  THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND
+//  DANIEL W. MCROBB HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+//  UPDATES, ENHANCEMENTS, OR MODIFICATIONS. DANIEL W. MCROBB MAKES NO
+//  REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER
+//  IMPLIED OR EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE,
+//  OR THAT THE USE OF THIS SOFTWARE WILL NOT INFRINGE ANY PATENT,
+//  TRADEMARK OR OTHER RIGHTS.
+//===========================================================================
+
+//---------------------------------------------------------------------------
+//!  \file TestRipeAsnTxt.cc
+//!  \author Daniel W. McRobb
+//!  \brief Unit tests for Dwm::McCurtain::RipeAsnTxt
+//---------------------------------------------------------------------------
+
+#include <iostream>
+#include <string>
+
+#include "DwmSysLogger.hh"
+#include "DwmUnitAssert.hh"
+#include "DwmMcCurtainRipeAsnTxt.hh"
+
+using namespace std;
+using namespace Dwm;
+
+//----------------------------------------------------------------------------
+//!  
+//----------------------------------------------------------------------------
+static bool TestLoad(Dwm::McCurtain::RipeAsnTxt & asntxt, const string & path)
+{
+  bool  rc = false;
+  if (UnitAssert(asntxt.Load(path))) {
+    if (UnitAssert(asntxt.Entries().size() == 114498)) {
+      for (const auto & entry : asntxt.Entries()) {
+        UnitAssert(! entry.second.Name().empty());
+        UnitAssert(entry.second.CountryCode().size() == 2);
+      }
+    }
+  }
+  return rc;
+}
+      
+//----------------------------------------------------------------------------
+//!  
+//----------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+  Dwm::SysLogger::Open("TestRipeAsnTxt", LOG_PERROR|LOG_PID, "user");
+
+  Dwm::McCurtain::RipeAsnTxt  asntxt;
+
+  TestLoad(asntxt, "inputs/asn.txt");
+  
+  if (Assertions::Total().Failed())
+    Assertions::Print(cerr, true);
+  else
+    cout << Assertions::Total() << " passed" << endl;
+
+  exit(0);
+  
+testFailed:
+  
+  Assertions::Print(cerr, true);
+  exit(1);
+}
